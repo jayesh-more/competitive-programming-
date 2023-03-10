205b7f5ee7c741d51aebd5d0be811f3d3f567f37
Jayesh More <65273915+jayesh-more@users.noreply.github.com>
2023-03-10 07:41:56 +0530

    Create Solution.java

---
+++ geeksforgeeks/Maximum Triplet product/Solution.java
@@ -0,0 +1,14 @@
+
+class Solution {
+
+    Long maxTripletProduct(Long arr[], int n)
+
+    {
+
+        Arrays.sort(arr);
+
+        return Math.max(arr[0] * arr[1] * arr [n - 1], arr[n - 3] * arr[n - 2] * arr[n - 1]);
+
+    }
+
+}
